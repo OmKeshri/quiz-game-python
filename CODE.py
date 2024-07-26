6b1b13326cc4c9bd793f917c9de452f5eae02338
OmKeshri <omkeshri21@gmail.com>
2024-07-26 12:55:03 +0530

    clean code

--- CODE.py
+++ CODE.py
@@ -30,25 +30,25 @@ def quiz_game():
 
     for i in range(1, 6):
         print("\nROUND %d" % i)
-        mixer.music.load("round%s.mp3" % i)
+        mixer.music.load("Sounds\\round%s.mp3" % i)
         mixer.music.play()
         time.sleep(1)
         for j in range(1, 3):
             ques = random.choice(list(questions.keys()))
             print("\nQues%d: %s" % (j, ques))
             right_answers[ques] = questions[ques]
-            mixer.music.load("question%d.mp3" % j)
+            mixer.music.load("Sounds\\question%d.mp3" % j)
             mixer.music.play()
             time.sleep(1)
 
-            mixer.music.load("%s.mp3" % ques)
+            mixer.music.load("Sounds\\%s.mp3" % ques)
             mixer.music.play()
 
             ans = input("Ans: ")
             if len(ans) == 0:
                 while len(ans) == 0:
                     print("Please write the answer!")
-                    mixer.music.load("please write the answer..mp3")
+                    mixer.music.load("Sounds\\please write the answer..mp3")
                     mixer.music.play()
                     time.sleep(1)
                     ans = input("Ans: ")
@@ -59,12 +59,12 @@ def quiz_game():
             if ans == questions[ques]:
                 points += 1
                 print("Congratulation! You gave the right answer.")
-                mixer.music.load("congrats.mp3")
+                mixer.music.load("Sounds\\congrats.mp3")
                 mixer.music.play()
                 time.sleep(2.4)
             else:
                 print("Sorry! You gave the wrong answer.")
-                mixer.music.load("sorry.mp3")
+                mixer.music.load("Sounds\\sorry.mp3")
                 mixer.music.play()
                 time.sleep(2.1)
                 pass
@@ -72,44 +72,45 @@ def quiz_game():
             questions.pop(ques)
 
     print("\nTHANK YOU FOR PLAYING THIS GAME!")
-    mixer.music.load("Thankyou.mp3")
+    mixer.music.load("Sounds\\Thankyou.mp3")
     mixer.music.play()
     time.sleep(3.2)
     print("YOUR POINTS:", points)
     print("\nRIGHT ANSWERS:", right_answers)
     print("Your ANSWERS: ", your_answers)
 
-    print("\nDEVELOPERS NAME:- \'OM KESHRI\', \'SURU PREM KUMAR\'")
+    print("\nDEVELOPERS NAME:- OM KESHRI")
 
 
 mixer.init()
 
 print("WELCOME TO THE QUIZ GAME!")
-mixer.music.load("Welcome to the Quiz Game.mp3")
+# mixer.music.load("Welcome to the Quiz Game.mp3")
+mixer.music.load("Sounds\\Welcome to the Quiz Game.mp3")
 mixer.music.play()
 time.sleep(1.8)
 
 print("PLEASE READ THE RULES BEFORE STARTING THE GAME!")
-mixer.music.load("Plz read the rules.mp3")
+mixer.music.load("Sounds\\Plz read the rules.mp3")
 mixer.music.play()
 time.sleep(2.9)
 
 print("RULE 1:-\nThere are 5 rounds in this game and in each round 2 "
       "random statements will be given to you; You have to\nanswer whether"
       " they are True or False; If your answer is correct, you will be rewarded by 1 point.")
-mixer.music.load("rule1.mp3")
+mixer.music.load("Sounds\\rule1.mp3")
 mixer.music.play()
 time.sleep(11.7)
 
 print("RULE 2:-\nPlease give the answer only in True or False otherwise "
       "your answer will consider as wrong.")
-mixer.music.load('rule2.mp3')
+mixer.music.load('Sounds\\rule2.mp3')
 mixer.music.play()
 time.sleep(6.8)
 
 while True:
     print("Type \'Ready\' to start the Game:")
-    mixer.music.load("Type ready.mp3")
+    mixer.music.load("Sounds\\Type ready.mp3")
     mixer.music.play()
     time.sleep(1)
 
@@ -118,14 +119,14 @@ while True:
         quiz_game()
         time.sleep(1)
         print("\nDO YOU WANT TO PLAY THE GAME AGAIN(YES/NO)?")
-        mixer.music.load("do you like to play the game again.mp3")
+        mixer.music.load("Sounds\\do you like to play the game again.mp3")
         mixer.music.play()
         time.sleep(1)
         b = input()
         if len(b) == 0:
             while len(b) == 0:
                 print("Please write the answer!")
-                mixer.music.load("please write the answer..mp3")
+                mixer.music.load("Sounds\\please write the answer..mp3")
                 mixer.music.play()
                 time.sleep(1)
                 b = input()
@@ -135,6 +136,6 @@ while True:
             break
     else:
         print("Please type Ready correctly!")
-        mixer.music.load("plz type ready correctly.mp3")
+        mixer.music.load("Sounds\\plz type ready correctly.mp3")
         mixer.music.play()
         time.sleep(1.7)
